fe11f49cd4cfd46c682fb88d5b49f3650e193d12
romedina <amerpad12@gmail.com>
2020-04-03 09:46:23 -0600

    [UPD] google analytics added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Body} from './components/Body/Body';
+import ReactGA from 'react-ga'
+
+// google analitycs
+ReactGA.initialize('UA-162708338-1')
+ReactGA.pageview(window.location.pathname)
 
 function App() {
   return (
